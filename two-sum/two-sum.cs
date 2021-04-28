9478e7e56793ab6a5211f8f762bf8862d3951a5c
nkaraffa <81177008+nkaraffa@users.noreply.github.com>
2021-04-27 21:05:30 -0700

    Time: 244 ms (45.44%), Space: 31.6 MB (89.23%) - LeetHub

---
+++ two-sum/two-sum.cs
@@ -0,0 +1,23 @@
+public class Solution 
+{    
+    public int[] TwoSum(int[] nums, int target) 
+    {
+        int [] answer = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    bool test = nums[i] + nums[j] == target;
+
+                    if (test)
+                    {
+                        answer[0] = i;
+                        answer[1] = j;
+                        return answer;
+                    }
+                }
+            }
+        return answer;
+    }
+}
